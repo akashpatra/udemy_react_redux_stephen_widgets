8a45728bfb64381bd2aa095739778101efef55a8
Akash Patra <patraakash9@gmail.com>
2020-09-16 18:57:11 +0530

    Executing the Request from useEffect

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
     const [term, setTerm] = useState('');
@@ -6,8 +7,20 @@ const Search = () => {
     console.log('I RUN WITH EVERY RENDER');
 
     useEffect(() => {
-        console.log('I RUN AFTER EVERY RENDER AND AT INITIAL RENDER');
-    }, []);
+        const search = async () => {
+            await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term,
+                }
+            });
+        }
+        
+        search()
+    }, [term]);
 
     return (
         <div>
