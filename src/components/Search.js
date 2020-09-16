565361eb6731e5448cda35e793eaf1a0865b42dc
Akash Patra <patraakash9@gmail.com>
2020-09-16 19:24:25 +0530

    Default Search Terms

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,12 +3,11 @@ import axios from 'axios';
 
 const Search = () => {
     const [term, setTerm] = useState('');
-
-    console.log('I RUN WITH EVERY RENDER');
+    const [results, setResults] = useState([]);
 
     useEffect(() => {
         const search = async () => {
-            await axios.get('https://en.wikipedia.org/w/api.php', {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
                 params: {
                     action: 'query',
                     list: 'search',
@@ -17,9 +16,12 @@ const Search = () => {
                     srsearch: term,
                 }
             });
+            setResults(data.query.search);
         }
         
-        search()
+        if (term) {
+            search();
+        }
     }, [term]);
 
     return (
