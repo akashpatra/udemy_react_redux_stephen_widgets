37d4b908db25da8b50b86b933ee5cf8782bcc950
Akash Patra <patraakash9@gmail.com>
2020-09-17 15:39:42 +0530

    Scaffolding the Dropdown

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 const items = [
     {
@@ -16,10 +17,25 @@ const items = [
         content: 'You use React by creating components'
     }
 ];
+
+const options = [
+    {
+        label: 'The Color Red',
+        value: 'red'
+    },
+    {
+        label: 'The Color Green',
+        value: 'green'
+    },
+    {
+        label: 'A shade of Blue',
+        value: 'blue'
+    }
+];
 export default () => {
     return (
         <div>
-            <Search />
+            <Dropdown options={options} />
         </div>
     );
 };
