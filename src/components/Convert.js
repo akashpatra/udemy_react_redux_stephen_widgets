957607a9cbb6e23ad1dcafe4b7f6dbf7ccf19440
Akash Patra <patraakash9@gmail.com>
2020-10-23 12:21:00 +0530

    Displaying Translated Text

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -2,20 +2,36 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Convert = ({ language, text }) => {
+  const [translated, setTranslated] = useState('');
+
   useEffect(() => {
-    axios.post(
-      'https://translation.googleapis.com/language/translate/v2',
-      {},
-      {
-        params: {
-          q: text,
-          target: language.value,
-          key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
-        },
-      }
-    );
+    // Helper Function to add async keyword. As, we can place directly in useEffect.
+    const doTranslation = async () => {
+      const { data } = await axios.post(
+        'https://translation.googleapis.com/language/translate/v2',
+        {},
+        {
+          params: {
+            q: text,
+            target: language.value,
+            key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
+          },
+        }
+      );
+
+      // First 'data' is of axios data and then next 'data' is of actual response we got from the api.
+      setTranslated(data.data.translations[0].translatedText);
+    };
+
+    // Invoke the helper Function
+    doTranslation();
   }, [language, text]);
-  return <div />;
+
+  return (
+    <div>
+      <h1 className="ui header">{translated}</h1>
+    </div>
+  );
 };
 
 export default Convert;
