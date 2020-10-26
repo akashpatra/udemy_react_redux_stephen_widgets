9a5d5aa5da6fcde1d82cb9c5c5a64bc987257a65
Akash Patra <patraakash9@gmail.com>
2020-10-26 19:31:07 +0530

    Detecting Navigation

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Link = ({ className, href, children }) => {
   const onClick = (event) => {
+    // Prevent the default functionality of browser to reload the whole index.html
     event.preventDefault();
+
+    // Change the URL
     window.history.pushState({}, '', href);
+
+    // To Notify all ahref that URL has been changed.
+    const navEvent = new PopStateEvent('popstate');
+    window.dispatchEvent(navEvent);
   };
 
   return (
--- src/components/Route.js
+++ src/components/Route.js
@@ -1,4 +1,20 @@
+import { useEffect } from 'react';
+
 const Route = ({ path, children }) => {
+  // Listener to listen change in URL
+  useEffect(() => {
+    const onLocationChange = () => {
+      console.log('Location Change');
+    };
+
+    // Add Event Listener
+    window.addEventListener('popstate', onLocationChange);
+
+    // CleanUp Function to remove the Listener when the component is removed from DOM.
+    return () => {
+      window.removeEventListener('popstate', onLocationChange);
+    };
+  }, []);
   return window.location.pathname === path ? children : null;
 };
 
