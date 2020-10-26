3029447b825615d727199c07a402acc00c362e53
Akash Patra <patraakash9@gmail.com>
2020-10-26 20:06:24 +0530

    Updating the Route

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,10 +1,14 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const Route = ({ path, children }) => {
+  // Setting initial state if the page is refreshed by the user, else it will show blank page.
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
   // Listener to listen change in URL
   useEffect(() => {
     const onLocationChange = () => {
-      console.log('Location Change');
+      // To reload the Route by changing the state
+      setCurrentPath(window.location.pathname);
     };
 
     // Add Event Listener
@@ -15,7 +19,11 @@ const Route = ({ path, children }) => {
       window.removeEventListener('popstate', onLocationChange);
     };
   }, []);
-  return window.location.pathname === path ? children : null;
+
+  // currentPath or window.location.pathname anything is fine.
+  // If, 'currentPath' then in the above state declaration, we are giving 'window.location.pathname' as initial state
+  // Else, 'window.location.pathname', no need to mention the initiat state in above state declaration
+  return currentPath === path ? children : null;
 };
 
 export default Route;
